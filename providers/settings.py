b813f457e5da262b4a66cd4b4287169719fa4ddd
raabyakhan <Sherlock129D>
2022-10-23 05:21:50 -0500

    testing db settings

--- providers/settings.py
+++ providers/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'providers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'providers',
-        'USER': 'provideradmin',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'providers',
+#         'USER': 'provideradmin',
+#         'PASSWORD': '123456789',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
